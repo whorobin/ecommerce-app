abd200c55e7696c238f07a5b810274722463fb5d
Zakaria Islam Robin <zakaria.pau49@gmail.com>
2024-09-23 12:17:07 +0530

    added navbar

---
+++ frontend/src/pages/Collection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Collection = () => {
+    return (
+        <div>
+            <h2>this is collection</h2>
+        </div>
+    );
+};
+
+export default Collection;
